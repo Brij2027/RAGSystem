fffc32c5c300dfc7e237136c53fdab81888d7850
Brij2027 <brijtiwari2027@gmail.com>
2024-12-23 17:29:53 +0530

    fixed open ai key not getting through

--- Dockerfile
+++ Dockerfile
@@ -29,5 +29,4 @@ RUN chmod +x /app/docker-entrypoint.sh
 
 EXPOSE 8000
 
-# CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
 ENTRYPOINT ["/app/docker-entrypoint.sh"]
--- app/core/answer_generator.py
+++ app/core/answer_generator.py
@@ -4,7 +4,6 @@ import os
 openai.api_key = os.getenv("OPEN_AI_KEY")
 
 def generate_answer_from_context(question: str, context: str):
-    print("OpenAI API Key:", os.getenv("OPEN_AI_KEY"))  # Ensure the key is printed for debugging
     response = openai.Completion.create(
         model="gpt-3.5-turbo",  # Specify the model you'd like to use
         prompt=f"Question: {question}\nContext: {context}\nAnswer:",
